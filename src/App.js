7963c78812dcf52e4254f2c4d40ff04ed7c6fb1a
OluwadaraJ <oluwadarajolaade@gmail.com>
2024-09-27 14:43:25 +0100

    added top nav bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Navbar from './features/header/topNav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar/>
+      
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
@@ -50,7 +52,7 @@ function App() {
             React Redux
           </a>
         </span>
-      </header>
+      
     </div>
   );
 }
