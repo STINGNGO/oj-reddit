ffcb33b4ab650cb91dc1cce04effabc3ecaec62b
OluwadaraJ <oluwadarajolaade@gmail.com>
2024-09-27 15:27:43 +0100

    added a state

--- src/features/content/contentBox.js
+++ src/features/content/contentBox.js
@@ -1,16 +1,28 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 
 export default function ContentBox() {
-    return (
-        <div  className="content-box">
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
 
-            <div  className="content">
-                <span>OJ</span>
-                
-            </div>
-                
-      </div>
-    );
+  useEffect(() => {
+    // simulate a data fetching process
+    setTimeout(() => {
+      setData('Loaded data!');
+      setLoading(false);
+    }, 2000);
+  }, []);
 
+  return (
+    <div className="content-box" style={{ backgroundColor: loading ? '#ccc' : '' }}>
+      {loading ? (
+        <div className="content">
+          <span>Loading...</span>
+        </div>
+      ) : (
+        <div className="content">
+          <span >{data}</span>
+        </div>
+      )}
+    </div>
+  );
 }
